fdf706e3f78fff95d2aae294604d46a5ec2455e0
Jason Nutter <jasondnutter@gmail.com>
2017-04-08 17:31:31 -0700

    Add babel-plugin-lodash

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
         require('babel-preset-react')
     ],
     plugins: [
+        require('babel-plugin-lodash'),
         require('react-hot-loader/babel'),
         require('babel-plugin-transform-object-rest-spread'),
         require('babel-plugin-transform-class-properties')
